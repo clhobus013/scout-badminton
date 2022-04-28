d6e52ca31c5e3baf6bd3a07e01304da7d6d84768
claudia <clauu013@gmail.com>
2022-04-27 21:16:50 -0300

    criacao projeto

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title = 'scout-badminton';
+
+  constructor(private http: HttpClient) {
+  }
+
+}
